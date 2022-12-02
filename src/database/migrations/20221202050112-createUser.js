fef2a82968d0a0b7e954c5137c5f239a0f51f779
quan.ta <quan.ta@geekup.vn>
2022-12-02 12:42:23 +0700

    change first migration

--- src/database/migrations/20221202050112-createUser.js
+++ src/database/migrations/20221202050112-createUser.js
@@ -1,13 +1,65 @@
 module.exports = {
   async up(db, client) {
-    await db.createCollection('student');
-    await db.createCollection('facultySecretary');
-    await db.createCollection('facultyViceDean');
+    await db.createCollection('student')
+      .then(() => {
+        console.log("Collection student has been created!!!");
+          return Promise.resolve();
+        })
+      .catch((err) => {
+        console.log(err);
+          return err;
+        });
+
+    await db.createCollection('facultySecretary')
+      .then(() => {
+        console.log("Collection facultySecretary has been created!!!");
+          return Promise.resolve();
+        })
+      .catch((err) => {
+        console.log(err);
+          return err;
+        });
+
+    await db.createCollection('facultyViceDean')
+      .then(() => {
+        console.log("Collection facultyViceDean has been created!!!");
+          return Promise.resolve();
+        })
+      .catch((err) => {
+        console.log(err);
+          return err;
+        });
   },
 
   async down(db, client) {
-    await db.dropCollection('ftudent');
-    await db.dropCollection("facultySecretary");
-    await db.dropCollection("facultyViceDean");
+    await db.dropCollection('student')
+      .then(() => {
+        console.log("Collection student has been dropped!!!");
+          return Promise.resolve();
+        })
+      .catch((err) => {
+        console.log(err);
+          return err;
+        });
+
+    await db.dropCollection('facultySecretary')
+      .then(() => {
+        console.log("Collection facultySecretary has been dropped!!!");
+          return Promise.resolve();
+        })
+      .catch((err) => {
+        console.log(err);
+          return err;
+        });
+
+    await db.dropCollection('facultyViceDean')
+      .then(() => {
+        console.log("Collection facultyViceDean has been dropped!!!");
+          return Promise.resolve();
+        })
+      .catch((err) => {
+        console.log(err);
+          return err;
+        });
   }
 };
